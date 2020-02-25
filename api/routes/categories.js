45f0b4dbe61892886dffdf9106ec3b7e98733714
Adil Abdu Bushra <adilabdu68@gmail.com>
2020-02-25 13:55:32 +0300

    Added API Routes middleware for Categories

---
+++ api/routes/categories.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "GET All Categories"
+    });
+});
+
+router.post('/', (req, res, next) => {
+
+    // Example of Parsing JSON request
+    let category = {
+        categoryName: req.body.categoryName
+    };
+
+    res.status(201).json({
+        message: "POST a Category",
+        category: category
+    });
+});
+
+router.patch('/:categoryID', (req, res, next) => {
+    res.status(200).json({
+        message: "PATCH request",
+        ID: req.params.categoryID
+    });
+});
+
+router.delete('/:categoryID', (req, res, next) => {
+    res.status(404).json({
+        message: "DELETE request",
+        ID: req.params.categoryID
+    })
+});
+
+module.exports = router;
