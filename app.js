ac54400122976f19b83e737f11298b955c29964b
Adil Abdu Bushra <adilabdu68@gmail.com>
2020-02-25 15:55:13 +0300

    Updated the CORS Error Handling method, fixed error in performing API requests.

--- app.js
+++ app.js
@@ -7,19 +7,22 @@ const bodyParser = require('body-parser');
 const bidRoutes = require('./api/routes/bids');
 
 // dev tools
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // CORS Error Handling
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, ' +
-        'Content-Type, Accept, Authorization');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
     if(req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
+    next();
 });
 
 // Routes
