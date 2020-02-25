ca8bfba3332948138f6b93be72da504900ccbaa1
honeypie404 <hannaadugna37@gmail.com>
2020-02-25 13:36:21 +0100

    category updated

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
 
     // Example of Parsing JSON request
-    let category = {
+    const category = {
         categoryName: req.body.categoryName
     };
 
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 // Route Handler Middleware initialization
 const bidRoutes = require('./api/routes/bids');
+const categoryRoutes = require('./api/routes/categories');
 
 // dev tools
 app.use(morgan('dev'));
@@ -12,18 +13,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // CORS Error Handling
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, ' +
-        'Content-Type, Accept, Authorization');
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, ' +
+//         'Content-Type, Accept, Authorization');
+//     if(req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//         return res.status(200).json({});
+//     }
+// });
 
 // Routes
 app.use('/bids', bidRoutes);
+app.use('/categories',categoryRoutes);
 
 // Error Handlers
 app.use((req, res, next) => {
