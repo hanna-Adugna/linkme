9c921379a9910d4c3cc31bd1646faceafdcc1d1f
Adil Abdu Bushra <adilabdu68@gmail.com>
2020-02-26 13:56:08 +0300

    Removed Error

--- app.js
+++ app.js
@@ -7,37 +7,6 @@ const bodyParser = require('body-parser');
 const bidRoutes = require('./api/routes/bids');
 const categoryRoutes = require('./api/routes/categories');
 
-// dev tools
-// app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// CORS Error Handling
-<<<<<<< HEAD
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, ' +
-//         'Content-Type, Accept, Authorization');
-//     if(req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//         return res.status(200).json({});
-//     }
-// });
-=======
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-});
->>>>>>> c108435a95137af4499dc2d9efe7780f12db2b43
-
 // Routes
 app.use('/bids', bidRoutes);
 app.use('/categories',categoryRoutes);
