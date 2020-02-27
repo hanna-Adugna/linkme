12e13326b97f0c90e3e4a83b30d9d707ea79775f
honeypie404 <hannaadugna37@gmail.com>
2020-02-27 21:47:25 +0100

    job crud done

--- api/routes/jobs.js
+++ api/routes/jobs.js
@@ -6,17 +6,21 @@ const Job = require('../models/job.model');
 
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "GET All Jobs"
+    Job.find().exec()
+    .then(docs =>{
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
+ 
 });
 
 router.post('/', (req, res, next) => {
-    // Example of Parsing JSON request
-    // let job = {
-    //     title: req.body.title,
-    //     jobLength: req.body.jobLength
-    // };
     const job = new Job({
         _id: new mongoose.Types.ObjectId(),
         title: req.body.title,
@@ -24,29 +28,73 @@ router.post('/', (req, res, next) => {
     });
     job
     .save().
-    then(result => {
+    then(result => { 
         console.log(result);
+        res.status(201).json({
+            message: "POST a Job",
+            postedJob: result
     })
-    .catch(err => console.log(err));
-    
-    res.status(201).json({
-        message: "POST a Job",
-        postedJob: job
+})
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err 
+        });
     });
+});
+router.get("/:jobID",(req,res,next) => {
+    const id = req.params.jobID;
+    Job.findById(id).exec()
+    .then(doc => {
+        console.log("From databse",doc);
+        if(doc){
+            res.status(200).json(doc);
+        }
+        else{
+            res.status(404)
+            .json({message: 'No valid entry found'});
+        }
+        res.status(200).json(doc);
+ })
+ .catch(err => {
+    console.log(err);
+    res.status(500).json({error:err});  
+ });
 });
 
 router.patch('/:jobID', (req, res, next) => {
-    res.status(200).json({
-        message: "PATCH request",
-        ID: req.params.jobID
-    });
+  const id = req.params.jobID;
+  const updateOps = {};
+  for (const ops of req.body) {
+      updateOps[ops.propName] = ops.value;
+  }
+  Job.update({_id: id},{ $set: updateOps })
+  .exec()
+  .then(result => {
+    console.log(result);
+    res.status(200).json(result);
+  })
+  .catch(err => {
+   console.log(err);
+   res.status(500).json({ 
+   error: err
+      });
+  });
 });
 
 router.delete('/:jobID', (req, res, next) => {
-    res.status(404).json({
-        message: "DELETE request",
-        ID: req.params.jobID
-    })
+  const id = req.params.jobID;
+  Job.remove({_id: id})
+  .exec()
+  .then(result => {
+      res.status(200).json(result);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json({
+          error: err
+      });
+  });
 });
 
-module.exports = router;
+module.exports = router;    
